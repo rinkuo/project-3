2865a46e8f9a18518dad254c7dc6aafa45da1ff1
Damirbek Rustambekov <damirbekrustambekov@gmail.com>
2024-11-30 20:40:46 +0500

    done

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from students.views import student_list
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', student_list),
-]
