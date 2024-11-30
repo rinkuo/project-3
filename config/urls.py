0a358bab25f396e3f887f6a798991f9f2f821bce
AbuBakr <157690181+rinkuo@users.noreply.github.com>
2024-11-30 20:49:07 +0500

    Delete config directory

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from courses.views import courses_list
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', courses_list),
-]
